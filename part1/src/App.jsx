fee34ed561a34434665cf889db312ffea87b3ede
zenyhoj <joe.balingit@gmail.com>
2024-03-02 21:47:57 +0800

    adding some props

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -12,16 +12,8 @@ const App = () => {
 
   return (
     <div className="card">
-      <h1>
-        Hello there, {firstName}. Good you learn react at age {age}{" "}
-      </h1>
-      <p>
-        And you have abilities like {skill1} and {skill2}
-      </p>
       {/* props to be pass on the Hello component as an argument */}
-      <Hello name="Joe" />
-      <Hello name="Zen" />
-      <Hello name="Aloha" />
+      <Hello name={firstName} age={age} skill={skill1} skill2={skill2} />
     </div>
   );
 };
--- part1/src/Hello.jsx
+++ part1/src/Hello.jsx
@@ -1,7 +1,11 @@
 const Hello = (props) => {
+  console.log(props);
   return (
     <div>
-      <h1>Hello {props.name}</h1>
+      <h1>
+        Hello {props.name} and you are {props.age} and has a skill of{" "}
+        {props.skill} and {props.skill2}
+      </h1>
     </div>
   );
 };
